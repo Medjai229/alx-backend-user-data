1dc1cf20089eb8e1cc737b31ffc2fe02ce9ce713
Malik <malikhussein229@gmail.com>
2024-08-29 15:17:42 +0300

    Task#1 Log formatter

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -15,3 +15,19 @@ def filter_datum(fields: List[str], redaction: str, message: str, separator: str
         temp = re.sub(field + "=.*?" + separator, field + "=" + redaction + separator, temp)
 
     return temp
+
+class RedactingFormatter(logging.Formatter):
+    """
+    Redacting Formatter class
+    """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        return filter_datum(self.fields, self.REDACTION, super(RedactingFormatter, self).format(record), self.SEPARATOR)
