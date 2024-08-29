df42ae505fe5baf51955a01478e3b39078cee096
Malik <malikhussein229@gmail.com>
2024-08-29 18:38:57 +0300

    Task#6 Fixing Error

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -15,7 +15,7 @@ def hash_password(password: str) -> bytes:
     Hashes a password for storing.
     """
     salt = bcrypt.gensalt()
-    hashed_pw = bcrypt.hashpw(b'password', salt)
+    hashed_pw = bcrypt.hashpw(password.encode('utf-8'), salt)
     return hashed_pw
 
 
